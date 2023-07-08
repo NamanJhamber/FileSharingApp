a9957c7f47db16c2aac3b0af7153cf5b38d3144f
Naman <90602027+NamanJhamber@users.noreply.github.com>
2023-07-08 17:14:00 +0530

    Update client.py

--- Music Sharing App/client.py
+++ Music Sharing App/client.py
@@ -1,133 +1,127 @@
-#-----------Bolierplate Code Start -----
 import socket
 from threading import Thread
 from tkinter import *
 from tkinter import ttk
+import ftplib
+import os
+import ntpath #This is used to extract filename from path
+import time
+from ftplib import FTP
+from tkinter import filedialog
+from pathlib import Path
 
+#import winsound
+#Uncomment the beow two line to use pygame to allow mp3 files as well
+from playsound import playsound
+import pygame
+from pygame import mixer
 
-PORT  = 8080
+PORT  = 8050
 IP_ADDRESS = '127.0.0.1'
 SERVER = None
 BUFFER_SIZE = 4096
 
-
 name = None
 listbox =  None
-textarea= None
-labelchat = None
-text_message = None
+filePathLabel = None
+
+global song_counter
+song_counter = 0
 
 
 
-def connectToServer():
-    global SERVER
-    global name
-    global sending_file
-
-    cname = name.get()
-    SERVER.send(cname.encode())
-
-#-----------Bolierplate Code End -----
 
 
+def play():
+    global song_selected
+    song_selected=listbox.get(ANCHOR)
+    
+    pygame
+    mixer.init()
+    mixer.music.load('shared_files/'+song_selected)
+    mixer.music.play()
+    if(song_selected != ""):
+        infoLabel.configure(text="Now Playing: " +song_selected)
+    else:
+       infoLabel.configure(text="")
 
-def openChatWindow():
+def stop():
+    global song_selected
+    pygame
+    mixer.init()
+    mixer.music.load('shared_files/'+song_selected)
+    mixer.music.pause()
+    infoLabel.configure(text="")
 
-    # ---------Teacher Activity Start---
-    print("\n\t\t\t\tIP MESSENGER")
 
-    #Client GUI starts here
-    window=Tk()
+  
 
-    window.title('Messenger')
-    window.geometry("500x350")
+   
+    
 
-    global name
-    global listbox
-    global textarea
-    global labelchat
-    global text_message
+    
+        
+#Client GUI
+def musicWindow(): 
+    global song_counter
     global filePathLabel
-
-    namelabel = Label(window, text= "Enter Your Name", font = ("Calibri",10))
-    namelabel.place(x=10, y=8)
-
-    name = Entry(window,width =30,font = ("Calibri",10))
-    name.place(x=120,y=8)
-    name.focus()
-
-    connectserver = Button(window,text="Connect to Chat Server",bd=1, font = ("Calibri",10), command = connectToServer)
-    connectserver.place(x=350,y=6)
-
-    separator = ttk.Separator(window, orient='horizontal')
-    separator.place(x=0, y=35, relwidth=1, height=0.1)
-
-    labelusers = Label(window, text= "Active Users", font = ("Calibri",10))
-    labelusers.place(x=10, y=50)
-
-    listbox = Listbox(window,height = 5,width = 67,activestyle = 'dotbox', font = ("Calibri",10))
-    listbox.place(x=10, y=70)
-
+    global listbox
+    global infoLabel
+    
+    window=Tk()
+    window.title('Music Window')
+    window.geometry("300x350")
+    window.configure(bg='LightSkyBlue')
+    
+    selectlabel = Label(window, text= "Select Song",bg='LightSkyBlue', font = ("Calibri",8))
+    selectlabel.place(x=2, y=1)
+    
+    listbox = Listbox(window,height = 10,width = 39,activestyle = 'dotbox',bg='LightSkyBlue',borderwidth=2, font = ("Calibri",10))
+    listbox.place(x=10,y=18)
+    for file in os.listdir('shared_files'):
+        filename = os.fsdecode(file)
+        listbox.insert(song_counter, filename)
+        song_counter = song_counter + 1
+        
     scrollbar1 = Scrollbar(listbox)
     scrollbar1.place(relheight = 1,relx = 1)
     scrollbar1.config(command = listbox.yview)
+    
+    PlayButton=Button(window,text="Play", width=10,bd=1,bg='SkyBlue',font = ("Calibri",10), command = play)
+    PlayButton.place(x=30,y=200)
+    
+    Stop=Button(window,text="Stop",bd=1,width=10,bg='SkyBlue', font = ("Calibri",10), command = stop)
+    Stop.place(x=200,y=200)
 
-    connectButton=Button(window,text="Connect",bd=1, font = ("Calibri",10))
-    connectButton.place(x=282,y=160)
-
-    disconnectButton=Button(window,text="Disconnect",bd=1, font = ("Calibri",10))
-    disconnectButton.place(x=350,y=160)
-
-    refresh=Button(window,text="Refresh",bd=1, font = ("Calibri",10))
-    refresh.place(x=435,y=160)
-
-    # ---------Teacher Activity End---
-
-    #------- Student Activity Start -------
-    labelchat = Label(window, text= "Chat Window", font = ("Calibri",10))
-    labelchat.place(x=10, y=180)
-
-    textarea = Text(window, width = 67,height = 6,font = ("Calibri",10))
-    textarea.place(x=10,y=200)
-
-    scrollbar2 = Scrollbar(textarea)
-    scrollbar2.place(relheight = 1,relx = 1)
-    scrollbar2.config(command = listbox.yview)
-
-    attach=Button(window,text="Attach & Send",bd=1, font = ("Calibri",10))
-    attach.place(x=10,y=305)
-
-    text_message = Entry(window, width =43, font = ("Calibri",12))
-    text_message.pack()
-    text_message.place(x=98,y=306)
-
-    send=Button(window,text="Send",bd=1, font = ("Calibri",10))
-    send.place(x=450,y=305)
-
-    filePathLabel = Label(window, text= "",fg= "blue", font = ("Calibri",8))
-    filePathLabel.place(x=10, y=330)
-
-    #------- Student Activity Start -------
-
-
-    # Teacher Activity
+       
+    
+    Upload=Button(window,text="Upload",width=10,bd=1,bg='SkyBlue', font = ("Calibri",10))
+    Upload.place(x=30,y=300)
+    
+    Download =Button(window,text="Download",width=10,bd=1,bg='SkyBlue', font = ("Calibri",10))
+    Download.place(x=200,y=300)
+    
+    infoLabel = Label(window, text= "",fg= "blue",bg='SkyBlue', font = ("Calibri",8))
+    infoLabel.place(x=4, y=330)
+    
     window.mainloop()
-
-
-#----------- Boilerlate Code  Start---------
-
+    
 def setup():
     global SERVER
     global PORT
     global IP_ADDRESS
+    global song_counter
 
     SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     SERVER.connect((IP_ADDRESS, PORT))
 
-    #----------- Boilerlate Code  Start---------
+    musicWindow()
 
-    # Teacher Activity 1
-    openChatWindow()
-
+#Initiate Server Connection    
 setup()
-#kndjf
+
+
+   
+
+
+
